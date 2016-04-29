df318e4b5673e94af985146037fb6495f896f47f
mbusson <mbusson@users.noreply.github.com>
2016-04-29 18:06:23 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/ >
+	 <link rel="stylesheet" href="styles.css">
+	 <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+	<div id="loader-wrapper">
+	    <div id="loader"></div>
+	    <div class="loader-section section-left"></div>
+	    <div class="loader-section section-right"></div>
+	</div>
+
+	<div id="">
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="style.js"></script>
+</body>
+</html>
